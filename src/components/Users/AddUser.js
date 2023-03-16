aeb8df782fa088ae2c645e99a588c11ad7ce5205
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-03-16 23:45:26 +0530

    task6

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,35 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
 
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import ErrorModal from '../UI/ErrorModal';
-import classes from './AddUser.module.css';
-import Wrapper from '../Helpers/Wrapper';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import classes from "./AddUser.module.css";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredCollage, setEnteredCollage] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    //console logging ref
+    console.log(nameInputRef.current.value);
+    console.log(ageInputRef.current.value);
+
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).',
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
     if (+enteredAge < 1) {
       setError({
-        title: 'Invalid age',
-        message: 'Please enter a valid age (> 0).',
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    props.onAddUser(enteredUsername, enteredAge, enteredCollage);
+    setEnteredUsername("");
+    setEnteredAge("");
+    setEnteredCollage("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -39,6 +48,10 @@ const AddUser = (props) => {
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
+
+  const collageChangeHandler = (event) => {
+     setEnteredCollage(event.target.value);
+  }
 
   const errorHandler = () => {
     setError(null);
@@ -61,6 +74,7 @@ const AddUser = (props) => {
             type="text"
             value={enteredUsername}
             onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
@@ -68,6 +82,14 @@ const AddUser = (props) => {
             type="number"
             value={enteredAge}
             onChange={ageChangeHandler}
+            ref={ageInputRef}
+          />
+          <label htmlFor="collage">Collage name</label>
+          <input
+            id="collage"
+            type="text"
+            value={enteredCollage}
+            onChange={collageChangeHandler}
           />
           <Button type="submit">Add User</Button>
         </form>
